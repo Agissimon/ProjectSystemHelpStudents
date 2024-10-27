fa902bd0179c00234f0264af74a7a730801aaf11
Agissimon <caramel3355for@gmail.com>
2024-10-27 13:38:52 +0300

    the working page UpcomingTasks is made

---
+++ ProjectSystemHelpStudents/Helper/UserSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSystemHelpStudents.Helper
+{
+    internal class UserSession
+    {
+        public static int IdUser { get; set; }
+        public static string NameUser { get; set; }
+    }
+}
